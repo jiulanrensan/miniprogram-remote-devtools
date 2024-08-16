3f6b0b571dcfe5b007ead8c545cf9eb5bbab2544
20190225001 <839492148@qq.com>
2024-08-17 01:43:51 +0800

    feat: init server

--- src/server/ws/index.ts
+++ src/server/ws/index.ts
@@ -6,12 +6,16 @@ const wsRoute = {
   devtools: 'devtools',
   client: 'client'
 }
-const serverMap = {
+const serverMap: { [key: string]: WebSocketServer | null } = {
   [wsRoute.devtools]: null,
   [wsRoute.client]: null
 }
 
 export function initServer({ server }: { server: http.Server }) {
+  Object.assign(serverMap, {
+    [wsRoute.client]: initClientServer(),
+    [wsRoute.devtools]: initDevtoolsServer()
+  })
   server.on('upgrade', (request, socket, head) => {
     console.log('request', request.url)
     const { url } = request
@@ -19,36 +23,66 @@ export function initServer({ server }: { server: http.Server }) {
       socket.destroy()
       return
     }
-    // serverMap[url.includes(wsRoute.devtools) ? wsRoute.devtools : wsRoute.client] =
-    //   initWebsocketServer({ request, socket, head })
+    const wss = serverMap[url]
+    if (!wss) return
+    handleUpgrade({ request, socket, head, wss })
   })
 }
 
 function handleUpgrade({
   request,
   socket,
-  head
-  // wss
+  head,
+  wss
 }: {
   request: http.IncomingMessage
   socket: internal.Duplex
   head: Buffer
+  wss: WebSocketServer
 }) {
-  // wss.handleUpgrade(request, socket, head, (ws) => {
-  //   wss.emit('connection', ws, request)
-  // })
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request)
+  })
 }
 
-function initWebsocketServer() {
+function initClientServer() {
+  const wss = initWebsocketServer({
+    onMessage: (message) => {
+      console.log('received: %s', message)
+    }
+  })
+  return wss
+}
+
+function initDevtoolsServer() {
+  const wss = initWebsocketServer({
+    onMessage: (message) => {
+      console.log('received: %s', message)
+    }
+  })
+  return wss
+}
+
+function initWebsocketServer({
+  onMessage,
+  onError,
+  onClose
+}: {
+  onMessage: (message: string) => void
+  onError?: (error: Error) => void
+  onClose?: () => void
+}) {
   const wss = new WebSocketServer({ noServer: true })
   wss.on('connection', (ws) => {
-    ws.on('message', (message) => {
-      console.log('received: %s', message)
-    })
-    ws.on('error', console.error)
-    ws.on('close', () => {
-      console.log('disconnected')
-    })
+    ws.on('message', onMessage)
+    ws.on('error', onError || console.error)
+    ws.on(
+      'close',
+      onClose ||
+        (() => {
+          console.log('disconnected')
+        })
+    )
   })
   return wss
 }
