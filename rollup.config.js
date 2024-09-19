b58262517afad21add6570bcf367f9fadbd5d092
lijiacheng <839492148@qq.com>
2024-09-19 18:45:34 +0800

    build: sdk构建

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,44 @@
 import dts from "rollup-plugin-dts";
 import typescript from "rollup-plugin-typescript2";
 export default [
-  {
-    input: "packages/common/src/index.ts",
-    output: {
-      file: "packages/common/dist/index.js",
-      format: "es",
-      sourcemap: true,
-    },
-    plugins: [
-      typescript({
-        tsconfig: './packages/common/tsconfig.json'
-      }),
-    ],
-  },
-  {
-    input: "packages/common/src/index.ts",
-    output: {
-      file: "packages/common/dist/index.d.ts",
-      format: "es",
-    },
-    plugins: [dts()]
-  }
   // {
-  //   input: "src/wechat-remote-sdk/index.ts",
+  //   input: "packages/common/src/index.ts",
   //   output: {
-  //     file: "sdk/index.js",
+  //     file: "packages/common/dist/index.js",
   //     format: "es",
   //     sourcemap: true,
   //   },
   //   plugins: [
-  //     typescript(),
+  //     typescript({
+  //       tsconfig: './packages/common/tsconfig.json'
+  //     }),
   //   ],
   // },
   // {
-  //   input: "src/wechat-remote-sdk/index.ts",
+  //   input: "packages/common/src/index.ts",
   //   output: {
-  //     file: "sdk/index.d.ts",
+  //     file: "packages/common/dist/index.d.ts",
   //     format: "es",
   //   },
   //   plugins: [dts()]
-  // }
+  // },
+  {
+    input: "packages/wechat-remote-sdk/src/index.ts",
+    output: {
+      file: "packages/wechat-remote-sdk/dist/index.js",
+      format: "es",
+      sourcemap: true,
+    },
+    plugins: [
+      typescript(),
+    ],
+  },
+  {
+    input: "packages/wechat-remote-sdk/src/index.ts",
+    output: {
+      file: "packages/wechat-remote-sdk/dist/index.d.ts",
+      format: "es",
+    },
+    plugins: [dts()]
+  }
 ]
