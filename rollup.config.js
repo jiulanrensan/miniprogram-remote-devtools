7256e5eda847d65d3837e361ab87eccd932a5c1c
lijiacheng <839492148@qq.com>
2024-09-23 18:40:18 +0800

    build: rollup watch and build

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,60 @@
 import dts from "rollup-plugin-dts";
 import typescript from "rollup-plugin-typescript2";
-export default [
-  // {
-  //   input: "packages/common/src/index.ts",
-  //   output: {
-  //     file: "packages/common/dist/index.js",
-  //     format: "es",
-  //     sourcemap: true,
-  //   },
-  //   plugins: [
-  //     typescript({
-  //       tsconfig: './packages/common/tsconfig.json'
-  //     }),
-  //   ],
-  // },
-  // {
-  //   input: "packages/common/src/index.ts",
-  //   output: {
-  //     file: "packages/common/dist/index.d.ts",
-  //     format: "es",
-  //   },
-  //   plugins: [dts()]
-  // },
-  {
-    input: "packages/wechat-remote-sdk/src/index.ts",
-    output: {
-      file: "packages/wechat-remote-sdk/dist/index.js",
-      format: "es",
-      sourcemap: true,
-    },
-    plugins: [
-      typescript(),
-    ],
-  },
-  {
-    input: "packages/wechat-remote-sdk/src/index.ts",
-    output: {
-      file: "packages/wechat-remote-sdk/dist/index.d.ts",
-      format: "es",
-    },
-    plugins: [dts()]
-  }
-]
+
+/**
+ * 需要打包的package
+ */
+const packageList = [ 'common', 'wechat-remote-sdk' ]
+
+function isProd() {
+  return process.env.NODE_ENV === 'production'
+}
+
+function addWatch(opt, packageName) {
+  Object.assign(opt, {
+    watch: {
+      buildDelay: 1000,
+      include: `packages/${packageName}/**`,
+      exclude: [
+        `packages/${packageName}/dist/**`,
+        `packages/${packageName}/node_modules/**`
+      ]
+    }
+  })
+}
+
+function createConfig() {
+  const configList = []
+  packageList.forEach((packageName) => {
+    const buildOpt = {
+      input: `packages/${packageName}/src/index.ts`,
+      output: {
+        file: `packages/${packageName}/dist/index.js`,
+        format: "es",
+        sourcemap: true,
+      },
+      plugins: [
+        typescript({
+          tsconfig: `packages/${packageName}/tsconfig.json`
+        }),
+      ],
+    }
+    const dtsOpt = {
+      input: `packages/${packageName}/src/index.ts`,
+      output: {
+        file: `packages/${packageName}/dist/index.d.ts`,
+        format: "es",
+      },
+      plugins: [dts()]
+    }
+    if (!isProd()) {
+      addWatch(buildOpt, packageName)
+      addWatch(dtsOpt, packageName)
+    }
+    configList.push(buildOpt, dtsOpt)
+  })
+  console.log('NODE_ENV', process.env.NODE_ENV, JSON.stringify(configList));
+  return configList
+}
+
+export default createConfig()
