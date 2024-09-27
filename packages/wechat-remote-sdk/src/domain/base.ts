1ef7ecabd663060ce4d548f5ed1f6564ff85555e
lijiacheng <839492148@qq.com>
2024-09-27 11:26:37 +0800

    feat(wechat-remote-sdk): send enable method

--- packages/wechat-remote-sdk/src/domain/base.ts
+++ packages/wechat-remote-sdk/src/domain/base.ts
@@ -1,4 +1,6 @@
+import { Domain } from '@miniprogram-remote-devtools/common'
 import { ClientSocket } from '../clientSocket'
+import { originLog } from './Runtime'
 
 export type BaseOptions = {
   socket: ClientSocket
@@ -8,11 +10,22 @@ export class Base {
   constructor(options: BaseOptions) {
     const { socket } = options
     this.socket = socket
-    this.enable()
     this.init()
+    this.socket.onMessage((res) => {
+      const info = JSON.parse(res.data as string)
+      if (info && info.method) {
+        this.enable(info)
+      }
+    })
   }
   public init() {}
-  public enable() {}
+  public enable(info: { id: number; method: string }) {
+    const { id, method } = info
+    const doamins = Object.keys(Domain)
+    if (doamins.some((domain) => `${domain}.enable` === method)) {
+      this.send({ id })
+    }
+  }
   public send(data: any) {
     this.socket.send(JSON.stringify(data))
   }
